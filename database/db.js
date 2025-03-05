9e48d071507d9561ce00caeb20c03e956cfb0e96
Ebru <yerdebru@gmail.com>
2025-03-06 02:41:36 +0300

    db update

--- database/db.js
+++ database/db.js
@@ -3,7 +3,11 @@ require("dotenv").config()
 
 const connectDB = async () => { 
   try {
-    process.env.DATABASE_URL && await mongoose.connect(process.env.DATABASE_URL);
+    if (!process.env.DATABASE_URL) {
+      console.log("provide db url")
+      return;
+    }
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("Database connected successfully")
   } catch (error) {
     console.error("Database connection failed")
